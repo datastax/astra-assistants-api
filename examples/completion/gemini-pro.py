d8f63fc588a4cf04841a7b882d9559d3d790a1e8
phact <estevezsebastian@gmail.com>
2023-12-14 12:23:19 -0500

    Revert "RAG with gemini-pro LLM + ada-002 embeddings"
    This reverts commit 3a4b0b77918a8ef0dd35b6d7560080f7060aa834.

--- examples/completion/gemini-pro.py
+++ examples/completion/gemini-pro.py
@@ -8,12 +8,10 @@ load_dotenv("./.env")
 
 # you do have to pass a key because the client requires it but it doesn't have to be valid since we're using a third party LLM
 OPENAI_API_KEY="Fake key"
-ASTRA_DB_APPLICATION_TOKEN=os.getenv("ASTRA_DB_APPLICATION_TOKEN")
+ASTRA_DB_TOKEN=os.getenv("ASTRA_DB_TOKEN")
 
+GOOGLE_JSON_PATH=os.getenv("GOOGLE_JSON_PATH")
 GOOGLE_PROJECT_ID=os.getenv("GOOGLE_PROJECT_ID")
-
-# get a vertexai api key here https://ai.google.dev/tutorials/setup
-VERTEXAI_API_KEY=os.getenv("VERTEXAI_API_KEY")
 
 base_url=os.getenv("base_url", "https://open-assistant-ai.astra.datastax.com/v1")
 print(base_url)
@@ -22,13 +20,41 @@ client = OpenAI(
     base_url=base_url,
     api_key=OPENAI_API_KEY,
     default_headers={
-        "astra-api-token": ASTRA_DB_APPLICATION_TOKEN,
+        "astra-api-token": ASTRA_DB_TOKEN,
+        "embedding-model": "textembedding-gecko@002",
         "VERTEXAI-PROJECT": GOOGLE_PROJECT_ID,
-        "api-key" : VERTEXAI_API_KEY,
     }
 )
 
-model="palm/gemini-pro"
+print("Uploading file:")
+
+# Upload the JSON auth file
+# this will get stored in your astradb as plain text, make sure your db adequately secured.
+# you only need to do this once, then you can save your file.id and use it to connect your openai client
+file = client.files.create(
+    file=open(
+        GOOGLE_JSON_PATH,
+        "rb",
+    ),
+    purpose="auth",
+)
+
+print(file.id)
+
+del client
+
+client = OpenAI(
+    base_url=base_url,
+    api_key=OPENAI_API_KEY,
+    default_headers={
+        "astra-api-token": ASTRA_DB_TOKEN,
+        "embedding-model": "textembedding-gecko@002",
+        "VERTEXAI-PROJECT": GOOGLE_PROJECT_ID,
+        "google-application-credentials-file-id": file.id
+    }
+)
+
+model="gemini-pro"
 
 prompt = "Hi what is your favorite ice cream"
 response = client.chat.completions.create(
