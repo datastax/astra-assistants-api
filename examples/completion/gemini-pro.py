bacc95baf7ca36d5de644d92d3e04b0562660424
phact <estevezsebastian@gmail.com>
2023-12-14 12:24:50 -0500

    ASTRA_DB_APPLICATION_TOKEN

--- examples/completion/gemini-pro.py
+++ examples/completion/gemini-pro.py
@@ -8,7 +8,7 @@ load_dotenv("./.env")
 
 # you do have to pass a key because the client requires it but it doesn't have to be valid since we're using a third party LLM
 OPENAI_API_KEY="Fake key"
-ASTRA_DB_TOKEN=os.getenv("ASTRA_DB_TOKEN")
+ASTRA_DB_APPLICATION_TOKEN=os.getenv("ASTRA_DB_APPLICATION_TOKEN")
 
 GOOGLE_JSON_PATH=os.getenv("GOOGLE_JSON_PATH")
 GOOGLE_PROJECT_ID=os.getenv("GOOGLE_PROJECT_ID")
@@ -20,8 +20,7 @@ client = OpenAI(
     base_url=base_url,
     api_key=OPENAI_API_KEY,
     default_headers={
-        "astra-api-token": ASTRA_DB_TOKEN,
-        "embedding-model": "textembedding-gecko@002",
+        "astra-api-token": ASTRA_DB_APPLICATION_TOKEN,
         "VERTEXAI-PROJECT": GOOGLE_PROJECT_ID,
     }
 )
@@ -47,8 +46,7 @@ client = OpenAI(
     base_url=base_url,
     api_key=OPENAI_API_KEY,
     default_headers={
-        "astra-api-token": ASTRA_DB_TOKEN,
-        "embedding-model": "textembedding-gecko@002",
+        "astra-api-token": ASTRA_DB_APPLICATION_TOKEN,
         "VERTEXAI-PROJECT": GOOGLE_PROJECT_ID,
         "google-application-credentials-file-id": file.id
     }
