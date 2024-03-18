5cdd9a97825ff4f476ddcfb494153fc69add7c30
phact <estevezsebastian@gmail.com>
2024-03-17 21:00:58 -0400

    make function calling example stream

--- examples/python/function_calling/basic.py
+++ examples/python/function_calling/basic.py
@@ -2,15 +2,21 @@ import time
 from openai import OpenAI
 from dotenv import load_dotenv
 from streaming_assistants import patch
+from openai.lib.streaming import AssistantEventHandler
+from typing_extensions import override
+from openai.types.beta.threads.runs import ToolCall
+import logging
+
+logger = logging.getLogger(__name__)
 
 load_dotenv("./.env")
 
 client = patch(OpenAI())
 
 # Ensure the right environment variables are configured for the model you are using
-#model="gpt-4-1106-preview"
+model="gpt-4-1106-preview"
 #model="anthropic/claude-3-opus-20240229"
-model="anthropic/claude-3-sonet-20240229"
+#model="anthropic/claude-3-sonnet-20240229"
 #model="gpt-3.5-turbo"
 #model="cohere/command"
 #model="perplexity/mixtral-8x7b-instruct"
@@ -63,42 +69,41 @@ thread = client.beta.threads.create()
 client.beta.threads.messages.create(
     thread_id=thread.id, role="user", content=user_message
 )
-run = client.beta.threads.runs.create(
+
+class EventHandler(AssistantEventHandler):
+    def __init__(self):
+        super().__init__()
+
+    @override
+    def on_exception(self, exception: Exception):
+        logger.error(exception)
+        raise exception
+
+    @override
+    def on_tool_call_done(self, toolCall: ToolCall):
+        logger.debug(toolCall)
+        tool_outputs = []
+        tool_outputs.append({"tool_call_id": toolCall.id, "output": "75 degrees F and sunny"})
+
+        with client.beta.threads.runs.submit_tool_outputs_stream(
+            thread_id=self.current_run.thread_id,
+            run_id=self.current_run.id,
+            tool_outputs=tool_outputs,
+            event_handler=EventHandler(),
+        ) as stream:
+            #for part in stream:
+            #    logger.info(part)
+            for text in stream.text_deltas:
+                print(text, end="", flush=True)
+            print()
+
+with client.beta.threads.runs.create_and_stream(
     thread_id=thread.id,
     assistant_id=assistant.id,
-)
-print(thread)
-print(run)
+    event_handler=EventHandler()
+) as stream:
+    stream.until_done()
+    #for part in stream:
+    #    logger.info(f"event: {part}")
 
-def wait_on_run(run, thread):
-    while run.status == "queued" or run.status == "in_progress" or run.status == "generating":
-        run = client.beta.threads.runs.retrieve(
-            thread_id=thread.id,
-            run_id=run.id,
-        )
-        time.sleep(0.5)
-    print(f"run {run}")
-    return run
-
-
-run = wait_on_run(run, thread)
-if run.required_action is not None:
-    print(run.required_action)
-    tool_outputs = []
-    for tool_call in run.required_action.submit_tool_outputs.tool_calls:
-        tool_outputs.append({"tool_call_id": tool_call.id, "output": "75 and sunny"})
-
-    run = client.beta.threads.runs.submit_tool_outputs(
-      thread_id=thread.id,
-      run_id=run.id,
-      tool_outputs=tool_outputs
-    )
-    run = wait_on_run(run, thread)
-
-messages = client.beta.threads.messages.list(thread_id=thread.id)
-print(f"{model}-->")
-print(messages.data[0].content[0].text.value)
-
-response = client.beta.threads.messages.list(thread_id=thread.id, stream=True)
-for part in response:
-    print(f"streamed response: {part.data[0].content[0].delta.value}")
+logger.info(thread)
