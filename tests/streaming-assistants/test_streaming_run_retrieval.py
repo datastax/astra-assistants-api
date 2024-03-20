c1f140d7eb1971ae72288bded7228e102349dee8
phact <estevezsebastian@gmail.com>
2024-03-20 14:17:19 -0400

    tests

--- tests/streaming-assistants/test_streaming_run_retrieval.py
+++ tests/streaming-assistants/test_streaming_run_retrieval.py
@@ -1,6 +1,11 @@
+import json
+
 import pytest
 import logging
 import time
+
+from openai.lib.streaming import AssistantEventHandler
+from typing_extensions import override
 
 logger = logging.getLogger(__name__)
 
@@ -31,20 +36,46 @@ def run_with_assistant(assistant, client):
     )
     logger.info(f"> {user_message}")
 
+    class EventHandler(AssistantEventHandler):
+        def __init__(self):
+            super().__init__()
+            self.on_text_created_count = 0
+            self.on_text_delta_count = 0
+
+        @override
+        def on_run_step_done(self, run_step) -> None:
+            print("retrieval")
+            matches = []
+            for tool_call in run_step.step_details.tool_calls:
+                matches = tool_call.retrieval
+                print(json.dumps(tool_call.retrieval))
+            assert len(matches) > 0
+
+        @override
+        def on_text_created(self, text) -> None:
+            # Increment the counter each time the method is called
+            self.on_text_created_count += 1
+            print(f"\nassistant > {text}", end="", flush=True)
+
+        @override
+        def on_text_delta(self, delta, snapshot):
+            # Increment the counter each time the method is called
+            self.on_text_delta_count += 1
+            print(delta.value, end="", flush=True)
+
+    event_handler = EventHandler()
+
     logger.info(f"creating run")
-    run = client.beta.threads.runs.create(
-        thread_id=thread.id,
-        assistant_id=assistant.id,
-        stream=True,
-    )
+    with client.beta.threads.runs.create_and_stream(
+            thread_id=thread.id,
+            assistant_id=assistant.id,
+            event_handler=event_handler,
+    ) as stream:
+        for part in stream:
+            print(part)
 
-    i = 0
-    for event in run:
-        i += 1
-        logger.info(f"{event}")
-    logger.info("\n")
-    assert i > 0
-
+    assert event_handler.on_text_created_count > 0
+    assert event_handler.on_text_delta_count > 0
 
 
 instructions = "You are a personal math tutor. Answer thoroughly. The system will provide relevant context from files, use the context to respond."
