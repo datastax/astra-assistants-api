81c800a28dfb242ffe72d0811b86085ae6500edc
phact <estevezsebastian@gmail.com>
2024-06-14 14:28:55 -0400

    conftests, don't start server in client tests

--- client/tests/streaming-assistants/conftest.py
+++ client/tests/streaming-assistants/conftest.py
@@ -1,17 +1,13 @@
-import asyncio
 import os
 import logging
-import threading
 import time
 
 import pytest
 import requests
-import uvicorn
 from dotenv import load_dotenv
 from openai import OpenAI
 from streaming_assistants import patch
 
-from impl.main import app
 
 load_dotenv("./../../.env")
 load_dotenv("./.env")
@@ -33,34 +29,7 @@ class SafeLoggingHandler(logging.StreamHandler):
 
 
 @pytest.fixture(scope="module")
-def start_application():
-    stop_event = threading.Event()
-
-    def run_server(this_stop_event):
-        config = uvicorn.Config(app=app, host="127.0.0.1", port=8000, log_level="debug")
-        server = uvicorn.Server(config=config)
-        loop = asyncio.new_event_loop()
-        asyncio.set_event_loop(loop)
-
-        async def serve_app():
-            await server.serve()
-
-        async def check_for_stop():
-            while not this_stop_event.is_set():
-                await asyncio.sleep(1)  # Check for the stop event every second
-            root_logger = logging.getLogger()
-            root_logger.setLevel(logging.CRITICAL)
-            await server.shutdown()  # Initiate server shutdown
-            if loop.is_running():
-                loop.stop()  # Stop the event loop if it's still running
-
-        loop.create_task(serve_app())
-        loop.create_task(check_for_stop())
-        loop.run_forever()
-
-    server_thread = threading.Thread(target=run_server, args=(stop_event,))
-    server_thread.start()
-
+def wait_for_server():
     # Wait for the server to be up
     timeout = 20  # seconds
     start_time = time.time()
@@ -80,15 +49,9 @@ def start_application():
 
     yield  # Tests run here
 
-    # After the tests, signal the server thread to stop and wait for it to finish
-    stop_event.set()
-    server_thread.join(timeout=100)
-    if server_thread.is_alive():
-        print("Server thread did not exit cleanly.")
-
 
 @pytest.fixture(scope="function")
-def streaming_assistants_openai_client(start_application) -> OpenAI:
+def streaming_assistants_openai_client(wait_for_server) -> OpenAI:
     oai = patch(OpenAI())
     #oai = OpenAI()
     return oai
