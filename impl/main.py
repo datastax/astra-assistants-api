f13e45eb22237d8985d5a0697c733fc4847eaeb9
phact <estevezsebastian@gmail.com>
2024-08-07 12:56:58 -0400

    loguru config

--- impl/main.py
+++ impl/main.py
@@ -1,4 +1,5 @@
 import asyncio
+import inspect
 import logging
 import os
 import sys
@@ -27,23 +28,25 @@ cass_logger = logging.getLogger('cassandra')
 cass_logger.setLevel(logging.WARN)
 
 class InterceptHandler(logging.Handler):
-    def emit(self, record):
-        # Get corresponding Loguru level if it exists
+    def emit(self, record: logging.LogRecord) -> None:
+        # Get corresponding Loguru level if it exists.
+        level: str | int
         try:
             level = logger.level(record.levelname).name
         except ValueError:
             level = record.levelno
 
-        # Find caller from where originated the logged message
-        frame, depth = logging.currentframe(), 2
-        while frame.f_code.co_filename == logging.__file__:
+        # Find caller from where originated the logged message.
+        frame, depth = inspect.currentframe(), 0
+        while frame and (depth == 0 or frame.f_code.co_filename == logging.__file__):
             frame = frame.f_back
             depth += 1
 
         logger.opt(depth=depth, exception=record.exc_info).log(level, record.getMessage())
 
+logging.basicConfig(handlers=[InterceptHandler()], level=0, force=True)
 
-logging.root.handlers = [InterceptHandler()]
+#logging.root.handlers = [InterceptHandler()]
 logging.root.setLevel("DEBUG")
 
 for name in logging.root.manager.loggerDict.keys():
