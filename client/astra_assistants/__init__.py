9ac3a7c262f9e1469cec69c98b0a36e3b07a898c
phact <estevezsebastian@gmail.com>
2024-08-24 01:24:38 -0400

    UX improvement, set openai key to dummy if it's not set

--- client/astra_assistants/__init__.py
+++ client/astra_assistants/__init__.py
@@ -1,2 +1,13 @@
+import logging
+import os
+
 from .patch import patch
+
+logger = logging.getLogger(__name__)
+
+if "OPENAI_API_KEY" not in os.environ:
+    logger.debug("OPENAI_API_KEY is unset. Setting it to 'dummy' so openai doesn't kill the "
+                 "process when using other LLMs.\nIf you are using OpenAI models (including for embeddings)"
+                 ", remember to set OPENAI_API_KEY to your actual key.")
+    os.environ["OPENAI_API_KEY"] = "dummy"
 
